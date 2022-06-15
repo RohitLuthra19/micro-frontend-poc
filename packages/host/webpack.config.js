266967be3cadb4b3045ab3ebd287c3b88a47a1e0
Rohit <rohit.luthra19@gmail.com>
2022-06-15 16:53:10 +0530

    updated container app to have module federation

--- packages/host/webpack.config.js
+++ packages/host/webpack.config.js
@@ -1,27 +1,75 @@
-const path = require('path');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack"); // only add this if you don't have yet
+const { ModuleFederationPlugin } = webpack.container;
+const deps = require("./package.json").dependencies;
+require("dotenv").config({ path: "./.env" });
 
-module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
+const buildDate = new Date().toLocaleString();
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  console.log({ isProduction });
+  return {
+    entry: "./src/index.js",
+    mode: process.env.NODE_ENV || "development",
+    devServer: {
+      port: 3000,
+      open: true,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
       },
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          loader: "babel-loader",
+          exclude: /node_modules/,
+          options: {
+            cacheDirectory: true,
+            babelrc: false,
+            presets: [
+              [
+                "@babel/preset-env",
+                { targets: { browsers: "last 2 versions" } },
+              ],
+              "@babel/preset-react",
+            ],
+            plugins: [
+              ["@babel/plugin-proposal-class-properties", { loose: true }],
+            ],
+          },
+        },
+      ],
+    },
+
+    plugins: [
+      new webpack.EnvironmentPlugin({ BUILD_DATE: buildDate }),
+      new webpack.DefinePlugin({
+        "process.env": JSON.stringify(process.env),
+      }),
+      new ModuleFederationPlugin({
+        name: "container",
+        remotes: {
+          app1: isProduction ? process.env.PROD_APP1 : process.env.DEV_APP1,
+          app2: isProduction ? process.env.PROD_APP2 : process.env.DEV_APP2,
+        },
+        shared: {
+          ...deps,
+          react: { singleton: true, eager: true, requiredVersion: deps.react },
+          "react-dom": {
+            singleton: true,
+            eager: true,
+            requiredVersion: deps["react-dom"],
+          },
+        },
+      }),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
-  devServer: {
-    static: path.resolve(__dirname, './dist'),
-    hot: true,
-  },
+  };
 };
